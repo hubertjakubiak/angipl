a420a8bf4b132206ee9fe6d91f5d056a9fcaab70
Hubert <hubert.jakubiak@gmail.com>
2016-06-11 18:36:19 +0200

    Install Devise

--- Gemfile
+++ Gemfile
@@ -48,3 +48,5 @@ gem "haml-rails", "~> 0.9"
 
 gem 'bootstrap-sass', '~> 3.3.6'
 gem 'sass-rails', '>= 3.2'
+
+gem 'devise'
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :users
+  root 'words#index'
   resources :words
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
