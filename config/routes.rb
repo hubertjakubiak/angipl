9a1482e93a9f3afc603cdbb00412a8ab078c57c7
Hubert <hubert.jakubiak@gmail.com>
2016-06-14 19:46:12 +0200

    Add name to users and display on table

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
-  get 'words/game' => 'words/game'
+  devise_for :users, :controllers => { registrations: 'registrations' }
+  get 'words/game' => 'words/game', as: 'game'
   root 'words#index'
   resources :words
   # The priority is based upon order of creation: first created -> highest priority.
