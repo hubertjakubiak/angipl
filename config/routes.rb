2a8b248a7229b68aa677b8326bf33265b5c0bb48
Hubert <hubert.jakubiak@gmail.com>
2016-06-14 19:54:01 +0200

    Add new link to words in nav

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { registrations: 'registrations' }
   get 'words/game' => 'words/game', as: 'game'
-  root 'words#index'
+  root 'words#game'
   resources :words
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
