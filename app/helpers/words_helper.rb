c4f1b8485cc9d156f8a405ef4a6b7c5a2643da54
Hubert <hubert.jakubiak@gmail.com>
2016-07-13 20:01:54 +0200

    Ready to push heroku

--- app/helpers/words_helper.rb
+++ app/helpers/words_helper.rb
@@ -2,12 +2,12 @@ module WordsHelper
 
   def good_answer
     @good_count = Stat.user_words(current_user.id).increment!(:good_count) if current_user
-    puts Word.where(:id => @correct_answer_id).first.increment!(:good_count)
+    Word.where(:id => @correct_answer_id).first.increment!(:good_count)
   end
 
   def bad_answer
     @bad_count = Stat.user_words(current_user.id).increment!(:bad_count) if current_user
-    puts Word.where(:id => @correct_answer_id).first.increment!(:bad_count)
+    Word.where(:id => @correct_answer_id).first.increment!(:bad_count)
   end
 
   def correctness(good_count, bad_count)
