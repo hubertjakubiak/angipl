e87257f81e7fc65a3977c769248237344c055f00
Hubert <hubert.jakubiak@gmail.com>
2016-07-28 20:46:39 +0200

    Add Ajax indicator to show loading

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,3 +1,11 @@
 $(document).ready(function(){
     $('[data-toggle="tooltip"]').tooltip();
 });
+
+ $(document).bind("ajaxSend", function(){
+   $("#loading").css('visibility', 'visible');
+ }).bind("ajaxComplete", function(){
+   $("#loading").css('visibility', 'hidden');
+ }).bind("ajaxError", function(){
+   $("#loading").html('Wystąpił błąd. Przeładuj stronę.')
+ });
