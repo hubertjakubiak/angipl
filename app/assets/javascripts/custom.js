646daf60fffff1a6532e415b08ee88451e10d53a
Hubert <hubert.jakubiak@gmail.com>
2016-07-29 18:41:27 +0200

    Make new div for ajax error loading message

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -7,6 +7,5 @@ $(document).ready(function(){
  }).bind("ajaxComplete", function(){
     $("#loading").css('visibility', 'hidden');
  }).bind("ajaxError", function(){
-    $("#loading").css('visibility', 'visible');
-    $("#loading").html('<span class="label label-danger">Wystąpił błąd. Proszę odświeżyć stronę.</span>')
+    $("#error-loading").html('<span class="label label-danger">Wystąpił błąd. Proszę odświeżyć stronę.</span>')
  });
