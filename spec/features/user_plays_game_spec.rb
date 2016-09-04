be07bcc3360cf859c709293abbac2a4740942f49
Hubert <hubert.jakubiak@gmail.com>
2016-09-04 19:47:15 +0200

    Spec for user selecting answer

--- spec/features/user_plays_game_spec.rb
+++ spec/features/user_plays_game_spec.rb
@@ -10,6 +10,13 @@ RSpec.feature "User plays game", type: :feature do
     visit root_path
   end
 
+  scenario 'and select answer', js: true do
+    click_button('Pokaż odpowiedzi')
+    first('.word.btn-default').click
+    #click_button("cat", visible: false);
+    expect(page).to have_css('.word.btn-success')
+  end
+
   scenario 'and select good answer', js: true do
     click_button('Pokaż odpowiedzi')
     find('.word[data-word="cat"]').click
