85be72d941aecd636443c78a3a4722c769d0562f
Hubert <hubert.jakubiak@gmail.com>
2016-09-14 20:32:41 +0200

    Hide avatar in registration profile

---
+++ spec/features/user_can_select_category_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.feature "User", type: :feature do
+  let!(:word) {FactoryGirl.create_list(:word, 10, :categories => [FactoryGirl.create(:category, name: 'Biznes')])}
+
+  background do
+    sign_in
+    visit root_path
+  end
+
+  scenario 'can select category', js: true do
+    find('#select-category').find(:xpath, 'option[2]').click
+    expect(page).to have_content('Biznes')
+    expect(page).to have_current_path(root_path(category: 'Biznes'))
+  end
+
+end
