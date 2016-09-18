a6bf783fbb771bc2e7042020c7d22081e44a6650
Hubert <hubert.jakubiak@gmail.com>
2016-09-18 11:22:09 +0200

    Integration test to check if category has not enough words

--- spec/features/user_can_select_category_spec.rb
+++ spec/features/user_can_select_category_spec.rb
@@ -1,17 +1,24 @@
 require 'rails_helper'
 
-RSpec.feature "User", type: :feature do
+RSpec.feature "User can select category", type: :feature do
   let!(:word) {FactoryGirl.create_list(:word, 10, :categories => [FactoryGirl.create(:category, name: 'Biznes')])}
+  let!(:category) {FactoryGirl.create(:category, name: 'Sport')}
 
   background do
     sign_in
     visit root_path
   end
 
-  scenario 'can select category', js: true do
+  scenario 'that exists and have words', js: true do
     find('#select-category').find(:xpath, 'option[text() = "Biznes"]').click
     expect(page).to have_content('Biznes')
     expect(page).to have_current_path(root_path(category: 'Biznes'))
+  end
+
+  scenario 'that exists but does not have words', js: true do
+    find('#select-category').find(:xpath, 'option[text() = "Sport"]').click
+    expect(page).to have_content('Kategoria musi zawierać minimum 5 słówek.')
+    expect(page).to have_current_path(root_path)
   end
 
 end
