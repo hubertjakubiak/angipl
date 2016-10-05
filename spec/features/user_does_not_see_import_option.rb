afb2681fda292a10e159e6d3c0d25a32d7c50913
Hubert <hubert.jakubiak@gmail.com>
2016-10-05 20:23:30 +0200

    Spec for import option

---
+++ spec/features/user_does_not_see_import_option.rb
@@ -0,0 +1,29 @@
+require 'spec_helper'
+
+RSpec.feature "User does not see import option", type: :feature do
+
+  let!(:word) {FactoryGirl.create_list(:word, 10, :categories => [FactoryGirl.create(:category)])}
+
+  background do
+    sign_in
+    expect(page).to have_content 'Wyloguj się'
+    visit root_path
+    click_link('Dodaj słówko')
+  end
+
+  scenario 'when signed in' do
+    expect(page).not_to have_content 'Importuj słówka'
+  end
+
+  scenario 'when signed in and he is admin' do
+    User.find(1).update(admin: true)
+    click_link('Dodaj słówko')
+    expect(page).to have_content 'Importuj słówka'
+  end
+
+  scenario 'when signed out' do
+    logout
+    expect(page).not_to have_content 'Importuj słówka'
+  end
+
+end
