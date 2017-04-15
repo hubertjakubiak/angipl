f8eea43a8bd154dcb1e03c01c1d4e509fdb05a53
Hubert Jakubiak <hubert.jakubiak@netguru.co>
2017-04-15 21:40:07 +0200

    Improve spec

--- spec/services/check_answer_spec.rb
+++ spec/services/check_answer_spec.rb
@@ -1,7 +1,10 @@
 require "rails_helper"
 
 describe CheckAnswer do
-  let!(:word) { create(:word, en: "cat", pl: "kot") }
+
+  shared_context "when there are some words" do
+    let!(:word) { create(:word, en: "cat", pl: "kot") }
+  end
 
   shared_examples "returns count of matched translations" do |value|
     it "returns count of matched translations" do
@@ -13,21 +16,44 @@ describe CheckAnswer do
     end
   end
 
-  context "when translation is correct" do
-    subject { described_class.new(en: "cat", pl: "kot").call }
-    
-    include_examples "returns count of matched translations", "1"
+  context "when there are some words" do
+    include_context "when there are some words"
+    context "when translation is correct" do
+      subject { described_class.new(en: "cat", pl: "kot").call }
+      
+      include_examples "returns count of matched translations", "1"
+    end
+
+    context "when translation is not correct" do
+      subject { described_class.new(en: "cat", pl: "pies").call }
+
+      include_examples "returns count of matched translations", "0"
+    end
+
+    context "when translation is not found" do
+      subject { described_class.new(en: "dog", pl: "pies").call }
+      
+      include_examples "returns count of matched translations", "0"
+    end
   end
 
-  context "when translation is not correct" do
-    subject { described_class.new(en: "cat", pl: "pies").call }
+  context "when there aren't any words" do
+    context "when translation is correct" do
+      subject { described_class.new(en: "cat", pl: "kot").call }
+      
+      include_examples "returns count of matched translations", "0"
+    end
 
-    include_examples "returns count of matched translations", "0"
-  end
+    context "when translation is not correct" do
+      subject { described_class.new(en: "cat", pl: "pies").call }
 
-  context "when translation is not found" do
-    subject { described_class.new(en: "dog", pl: "pies").call }
-    
-    include_examples "returns count of matched translations", "0"
+      include_examples "returns count of matched translations", "0"
+    end
+
+    context "when translation is not found" do
+      subject { described_class.new(en: "dog", pl: "pies").call }
+      
+      include_examples "returns count of matched translations", "0"
+    end
   end
 end
